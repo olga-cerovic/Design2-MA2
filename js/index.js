db72fef76bca687d2b55d3e7dd1abaa780f81359
olga-cerovic <83223832+olga-cerovic@users.noreply.github.com>
2022-06-02 16:26:30 +0200

    Copied from Design 2 MA1 repo

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const hamburgeMenuDiv = document.querySelector(".nav-menu-mobile");
+const hamLines = document.querySelectorAll(".ham-line");
+
+const handleHamburgerMenuButton = () => {
+  if (hamburgeMenuDiv.classList.contains("notShow")) {
+    hamburgeMenuDiv.classList.remove("notShow");
+    hamburgeMenuDiv.classList.add("show");
+  } else {
+    hamburgeMenuDiv.classList.remove("show");
+    hamburgeMenuDiv.classList.add("notShow");
+  }
+
+  console.log("ndfdsnmndmfnmsd");
+};
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 768) {
+    hamburgeMenuDiv.classList.add("notShow");
+    hamburgeMenuDiv.classList.remove("show");
+  }
+});
+
+hamburgeMenuDiv.addEventListener("click", handleHamburgerMenuButton);
