fb0403537a2e89a5850657df7928a5ea7b928709
olga-cerovic <olga.cerovic@stud.noroff.no>
2022-06-05 14:56:29 +0200

    Css
    Added styling to active class

---
+++ js/hamburger-menu.js
@@ -0,0 +1,23 @@
+const hamburgeMenuDiv = document.querySelector(".nav-menu-mobile");
+const hamIcon = document.querySelector(".hamburger-menu-icon");
+
+const handleHamburgerMenuButton = () => {
+  if (hamburgeMenuDiv.classList.contains("notShow")) {
+    hamburgeMenuDiv.classList.remove("notShow");
+    hamburgeMenuDiv.classList.add("show");
+  } else {
+    hamburgeMenuDiv.classList.remove("show");
+    hamburgeMenuDiv.classList.add("notShow");
+  }
+
+  console.log("ndfdsnmndmfnmsd");
+};
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 768) {
+    hamburgeMenuDiv.classList.add("notShow");
+    hamburgeMenuDiv.classList.remove("show");
+  }
+});
+
+hamburgeMenuDiv.addEventListener("click", handleHamburgerMenuButton);
